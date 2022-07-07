46b2e288f3e02698485067e0d1968eb660da6160
Mohamed Irfan <irfancuber@gmail.com>
2022-07-07 17:10:26 +0800

    Add handler for get all notes endpoint

--- backend/note-taking/lambda/getNotesHandler.py
+++ backend/note-taking/lambda/getNotesHandler.py
@@ -6,14 +6,15 @@ s3_client = boto3.client('s3')
 
 
 def lambda_handler(event, context):
+    user_email = event['queryStringParameters']['useremail']
     note_id = event['queryStringParameters']['noteid']
     file_content = s3_client.get_object(
         Bucket=S3_BUCKET_NAME,
-        Key=f'notes/{note_id}',
+        Key=f'notes/{user_email}/{note_id}.json',
     )["Body"].read()
 
     response_body = {
-      'data': str(file_content)[1:]
+      'data': file_content.decode('utf-8') 
     }
     
     return {
