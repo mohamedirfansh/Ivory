68b8d8991b05c4d4159caefe5b94c8ad85364f51
Mohamed Irfan <irfancuber@gmail.com>
2022-07-07 17:10:47 +0800

    Add handler for delete notes endpoint

---
+++ backend/note-taking/lambda/deleteNotesHandler.py
@@ -0,0 +1,19 @@
+import boto3
+import json
+
+S3_BUCKET_NAME = 'ivory-hackathon-g1'
+s3_client = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    user_email = event['queryStringParameters']['useremail']
+    note_id = event['queryStringParameters']['noteid']
+
+    response = s3_client.delete_object(
+      Bucket=S3_BUCKET_NAME,
+      Key=f'notes/{user_email}/{note_id}'
+    )
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
