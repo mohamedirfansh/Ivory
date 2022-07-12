b5131d32a63078175a608138c3ee665730a66f56
Mohamed Irfan <irfancuber@gmail.com>
2022-07-12 17:28:26 +0800

    Add notes

---
+++ backend/notes/lambda/deleteNotesHandler.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+
+S3_BUCKET_NAME = 'ivory-hackathon-g1'
+s3_client = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    user_email = event['queryStringParameters']['useremail']
+    note_id = event['queryStringParameters']['noteid']
+
+    response = s3_client.delete_object(
+      Bucket=S3_BUCKET_NAME,
+      Key=f'notes/{user_email}/{note_id}'
+    )
+    return {
+        'statusCode': 200,
+        'body': json.dumps(note_id), 
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'DELETE,OPTIONS,POST,GET'
+        }
+    }
---
+++ backend/notes/lambda/getNotesHandler.py
@@ -0,0 +1,28 @@
+import boto3
+import json
+
+S3_BUCKET_NAME = 'ivory-hackathon-g1'
+s3_client = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    user_email = event['queryStringParameters']['useremail']
+    note_id = event['queryStringParameters']['noteid']
+    file_content = s3_client.get_object(
+        Bucket=S3_BUCKET_NAME,
+        Key=f'notes/{user_email}/{note_id}.json',
+    )["Body"].read()
+
+    response_body = {
+      'data': file_content.decode('utf-8') 
+    }
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response_body),
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'DELETE,OPTIONS,POST,GET'
+        }
+    }
